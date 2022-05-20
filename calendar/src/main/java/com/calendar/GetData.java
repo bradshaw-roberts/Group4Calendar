a9e5a8b28dd8d25aa3c24171da2ed61b4ae5d579
broberts3 <104153641+bradshaw-roberts@users.noreply.github.com>
2022-05-20 02:57:23 +0000

    Added new fxml and a few other changes

--- calendar/src/main/java/com/calendar/GetData.java
+++ calendar/src/main/java/com/calendar/GetData.java
@@ -1,16 +1,58 @@
 package com.example.calendar;
 
-import java.io.FileReader;
-import java.io.IOException;
+import java.util.*;
+import java.util.Scanner;
+import java.io.*;  
+import java.io.File;
 
 public class GetData {
   //read and write to the files
-  public static void main(String[] args) {
-    try {
-      FileReader reader 
+
+  //basic layout of what methods we need in this java file, can be changed 
+  
+  public static void addEvent(Event event) {
+        //add the new event to the file
     }
 
+    public static void removeEvent(Event event) {
+        //remove the old event from the file
+    }
     
-  }
-  //get all events for a date  
+    public static void editEvent(Event oldEvent, Event newEvent) {
+        //this will call addEvent and send it the newEvent
+        //also call removeEvent and send it the oldEvent
+    }
+
+    public static void getAllEventsForDay(int date) {
+        //get all events on a certain date
+      String dateF = date + ".txt";
+      Scanner scan = new Scanner(new File(dateF));
+      while (scan.hasNextLine()) {
+        String line = scan.nextLine();
+        if(line == null) {
+          String title = scan.nextLine();
+          int start = scan.nextInt();
+          int end = scan.nextInt();
+          String location = scan.nextLine();
+          String notes = scan.nextLine();
+          
+        }else{}
+      }
+    }
+
+    public static void getAllEventsForWeek(int date, String dayOfTheWeek) {
+        /*
+        get all events for a week by calling getAllEventsForDay()
+        for every day in the week in which the given day resides.
+        */
+
+    }
+
+    public static void getAllEventsForMonth(int date, String dayOfTheWeek) {
+        /*
+        get all events for a week by calling getAllEventsForWeek()
+        for every week in the month in which the given day resides.
+        */
+    }
+   
 }
--- calendar/src/main/java/com/calendar/ToDoList.java
+++ calendar/src/main/java/com/calendar/ToDoList.java
@@ -1,9 +1,59 @@
 package com.example.calendar;
 
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class ToDoList() {
+
+  //basic layout of what methods we need in this java file, can be changed 
+  //read and write to ToDoList.txt in the resources file
   
+  public static ArrayList getAll() {
+    //return all items from the file in an ArrayList
+    ArrayList<String> items = new ArrayList<>();
+    Scanner fileReader = new Scanner("ToDoList.txt");
+    while (fileReader.hasNextLine()) {
+      items.add(fileReader.nextLine());
+      }
+  
+    return items;
+  }
+  
+  public static void add (String name) {
+    //given the name of the new to do list item add it to the file.
+    FileWriter listWriter = new FileWriter("ToDoList.txt");
+    listWriter.append(name + "\n");
+    listWriter.close();
+  }
+
+  public static void remove (String name) {
+    //given a string see if the item is in the file and if it is, remove it.
+    File importFile = new File("ToDoList.txt");
+    //Creates a new temp file, to later replace the original
+    File tempFile = new File("ToDoList.tmp");
+    BufferedReader br = new BufferedReader(new FileReader(importFile));
+    PrintWriter listWriter = new PrintWriter(tempFile);
+    String line = null;
+
+    //Reads the original ToDoList into the new temp one, excluding the
+    //line to be deleted
+    while ((line = br.readLine()) != null) {
+      if (!line.trim().equal(name)) {
+        listWriter.println(line);
+        listWriter.flush();
+      }
+    }
+    listWriter.close();
+    br.close();
+
+    if (!importFile.delete()) {
+      System.out.println("Could not delete file.");
+      return;
+    }
+
+    if (!tempFile.renameTo(importFile)) {
+      System.out.println("Could not rename temp file.");
+    }
+  }  
 }
