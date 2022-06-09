c83540a091b0a706b053c71795c0aabedaa3de0c
Bradshaw Roberts <bradshaw.roberts14@gmail.com>
2022-06-09 15:05:20 -0400

    test 4

--- src/main/java/com/group4calendar/CalendarController.java
+++ src/main/java/com/group4calendar/CalendarController.java
@@ -300,7 +300,6 @@ public class CalendarController implements Initializable  {
             weekViewDateLabels.get(i).setText(datesInWeek.get(i).getMonth().toString() + " " + datesInWeek.get(i).getDayOfMonth());
         }
     }
-    //testing git 1 2 3
 
     public void setUpWeekViewLabelsArrayList() {
         weekViewLabels.add(weekViewSUNDAYLabel);
